2bc9d9594693fd64c07e6ab2c4db56f882fc9b30
charles <128108973+OldFoolAgain@users.noreply.github.com>
2024-03-09 21:01:59 -0500

    initial

---
+++ ConnectionHistory/ConnectionStatus.cpp
@@ -0,0 +1,150 @@
+//Copyright © 2024 Charles Kerr. All rights reserved.
+
+#include "ConnectionStatus.hpp"
+
+#include <algorithm>
+#include <stdexcept>
+
+#include <fstream>
+#include <vector>
+
+#include "utility/strutil.hpp"
+using namespace std::string_literals ;
+
+//======================================================================
+ConnectionStatus::ConnectionStatus(const std::filesystem::path &filepath) : ConnectionStatus() {
+    if (!this->load(filepath)) {
+        throw std::runtime_error("Unable to load: "s + filepath.string());
+    }
+}
+
+//======================================================================
+auto ConnectionStatus::load(const std::filesystem::path &filepath) -> bool  {
+    connectionStatus.clear() ;
+    if (!std::filesystem::exists(filepath)) {
+        return false ;
+    }
+    lastRead = std::filesystem::last_write_time(filepath) ;
+    auto input = std::ifstream(filepath.string()) ;
+    if (!input.is_open()) {
+        std::cerr << "Unable to open: "s << filepath.string() << std::endl;
+        return false ;
+    }
+    ourfile = filepath ;
+    auto buffer = std::vector<char>(2048,0) ;
+    while (input.good() && !input.eof()) {
+        input.getline(buffer.data(), buffer.size()-1) ;
+        if (input.gcount()>0) {
+            buffer[input.gcount()] = 0 ;
+            std::string line = buffer.data() ;
+            line = util::trim(util::strip(line,"#")) ;
+            if (!line.empty()) {
+                auto [name,data] = util::split(line, "=") ;
+                auto iter = connectionStatus.find(name) ;
+                if (iter == connectionStatus.end()) {
+                    auto cont = LogContainer() ;
+                    cont.addData(data) ;
+                    connectionStatus.insert_or_assign(name,cont) ;
+                }
+                else {
+                    iter->second.addData(data) ;
+                }
+            }
+        }
+    }
+    input.close();
+    // Delete our copy of the file
+    // Now, lets sort them all
+    for (auto &[name,entry]:connectionStatus) {
+        entry.sort() ;
+    }
+    return true ;
+}
+
+//======================================================================
+auto ConnectionStatus::size() const -> size_t {
+    return connectionStatus.size() ;
+}
+//======================================================================
+auto ConnectionStatus::empty() const -> bool {
+    return connectionStatus.empty() ;
+}
+
+//======================================================================
+auto ConnectionStatus::keys() const -> std::vector<std::string> {
+    auto rvalue = std::vector<std::string>() ;
+    for (const auto &[name,value]:connectionStatus){
+        rvalue.push_back(name) ;
+    }
+    return rvalue ;
+}
+
+//======================================================================
+auto ConnectionStatus::contains(const std::string &name) const -> bool {
+    auto iter = connectionStatus.find(name) ;
+    return iter != connectionStatus.end() ;
+}
+
+//======================================================================
+auto ConnectionStatus::operator[](const std::string &key) const -> const LogContainer& {
+    auto iter = connectionStatus.find(key) ;
+    if (iter != connectionStatus.end()){
+        return iter->second ;
+    }
+    throw std::out_of_range("No status for entry: "s + key) ;
+}
+//======================================================================
+auto ConnectionStatus::operator[](const std::string &key)  -> LogContainer& {
+    auto iter = connectionStatus.find(key) ;
+    if (iter != connectionStatus.end()){
+        return iter->second ;
+    }
+    throw std::out_of_range("No status for entry: "s + key) ;
+
+}
+
+// ========================================================================
+auto ConnectionStatus::save(const std::filesystem::path &path, int hours) -> bool {
+    auto output = std::ofstream(path.string()) ;
+    if (!output.is_open()){
+        return false ;
+    }
+    auto time = util::ourclock::now() - std::chrono::hours(hours) ;
+    for (const auto &[name,value]:connectionStatus){
+        auto cont = LogContainer() ;
+        for (size_t i = 0 ; i < value.size();i++) {
+            if (value[i].timestamp >= time ) {
+                cont.addEntry(value[i]) ;
+            }
+        }
+        if (cont.empty()) {
+            // it was empty?
+            cont.addEntry(value.last()) ;
+        }
+        cont.sort() ;
+        if (cont.last().state) {
+            // The last state was "connected, so we need to add one
+            auto temp = cont.last() ;
+            temp.state = false ;
+            temp.timestamp = util::ourclock::now() ;
+            cont.addEntry(temp) ;
+            cont.sort() ;
+        }
+        // Now we write them out :
+        for (size_t i = 0 ; i < cont.size();i++) {
+            output << name << " = " << util::sysTimeToString(cont[i].timestamp) << " , " << (cont[i].state? "Connected , ":"Disconnected , ") << cont[i].address << std::endl; ;
+        }
+    }
+    return true ;
+}
+
+// ==============================================================================================================================
+auto ConnectionStatus::hasChanged() const -> bool {
+    auto lastwrite = std::filesystem::last_write_time(ourfile) ;
+    
+    if ( std::chrono::duration_cast<std::chrono::seconds>(lastwrite - lastRead).count() > 0 ){
+        return true ;
+    }
+    return false ;
+
+}
--- ConnectionHistory/main.cpp
+++ ConnectionHistory/main.cpp
@@ -1,14 +1,59 @@
-//
-//  main.cpp
-//  ConnectionHistory
-//
 //  Created by Charles Kerr on 3/9/24.
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <filesystem>
+#include <string>
+
+#include "ConnectionStatus.hpp"
+using namespace std::string_literals ;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    auto exitcode = EXIT_SUCCESS ;
+    
+    try {
+        auto connectionLog = std::filesystem::path() ;
+        auto clientName = std::string() ;
+        
+        if (argc <= 1) {
+            throw std::runtime_error("No connection log was provided.") ;
+        }
+        connectionLog = std::filesystem::path(argv[1]);
+        auto connectionStatus = ConnectionStatus(connectionLog);
+        if (argc <= 2) {
+            // we where given a connection log, but no client hname, give back all the client names available
+            std::cout << "No client name was provided, options are: " << std::endl;
+            auto keys = connectionStatus.keys() ;
+            for (const auto &entry: keys) {
+                std::cout << "\t" << entry << std::endl;
+            }
+            
+        }
+        else {
+            clientName = argv[2] ;
+            if (!connectionStatus.contains(clientName)) {
+                std::cout << "No history found for: " << clientName << std::endl;
+            }
+            else {
+                auto history = connectionStatus[clientName] ;
+                auto count = history.size() ;
+                std::cout << "There are " << count << " entries for: " << clientName << std::endl;
+                for (auto i = count -1; i >= 0; i -= 1 ) {
+                    std::cout << "\t" <<  history[i].describe() << std::endl;
+                }
+            }
+        }
+        
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        exitcode = EXIT_FAILURE ;
+    }
+    catch (...) {
+        std::cerr << "Unknown Error" << std::endl;
+        exitcode = EXIT_FAILURE ;
+    }
+    return exitcode;
 }
